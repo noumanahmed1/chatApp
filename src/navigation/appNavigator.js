cd2624a12d1580bc496ccf89c6b6a1aa4beab6f4
noumanahmed1 <noumanahmed6040@gmail.com>
2023-09-14 17:43:55 +0500

    initial comment

---
+++ src/navigation/appNavigator.js
@@ -0,0 +1,45 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import Splash from '../screens/splash';
+import Signup from '../screens/signUp';
+import Login from '../screens/login';
+import Main from '../screens/main';
+import Chat from '../screens/chat';
+const Stack = createStackNavigator();
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={'Splash'}
+          component={Splash}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name={'Signup'}
+          component={Signup}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name={'Login'}
+          component={Login}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name={'Main'}
+          component={Main}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name={'Chat'}
+          component={Chat}
+          options={{headerShown: true}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
